41a3e28234f61712d85ea322ce7107ab1f56d3a6
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2026-04-05 14:26:39 +0800

    style: use air

--- R/gc-ora.R
+++ R/gc-ora.R
@@ -215,7 +215,12 @@ enrich_gc_ora_kegg <- function(
       "motif" %in% colnames(tidy_dea_res) ~ "motif"
     )
     if (is.na(by)) {
-      required_cols <- c("glycan_structure", "glycan_composition", "trait", "motif")
+      required_cols <- c(
+        "glycan_structure",
+        "glycan_composition",
+        "trait",
+        "motif"
+      )
       cli::cli_abort(c(
         "Cannot determine glycan traits.",
         "i" = "At least one of these columns is needed: {.field {required_cols}}",
